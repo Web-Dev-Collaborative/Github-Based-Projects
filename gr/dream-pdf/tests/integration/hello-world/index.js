f65d065b5f4a224f80981cca55c1ca5600ba58fe
Bryan C Guner <bryan.guner@gmail.com>
2021-08-04 19:44:28 +0000

    update

---
+++ gr/dream-pdf/tests/integration/hello-world/index.js
@@ -0,0 +1,26 @@
+var test = require('tape')
+var fs = require('fs')
+
+var PDF = require('../../../pdf')
+
+test('hello world', function (t) {
+  t.plan(1)
+
+  var pdf = new PDF({
+    dimensions: {
+      width: 800,
+      height: 600
+    },
+    createdAt: new Date(Date.UTC(2015, 4, 9))
+  })
+
+  pdf.add('Hello,\nPDF world!')
+  pdf.getContent(function (error, content) {
+    if (error) throw error
+
+    var bufferGenerated = new Buffer(content, 'binary')
+    var bufferFixture = fs.readFileSync(__dirname + '/fixture.pdf')
+
+    t.equal(bufferGenerated.toString('binary'), bufferFixture.toString('binary'), 'should match fixture')
+  })
+})
---
+++ gr/einfachsmart.de/README.md
@@ -0,0 +1,6 @@
+einfachsmart.de
+===============
+
+A look behind the curtain of einfachsmart.de. 
+
+© 2012 Gregor Martynus. All rights reserved.
